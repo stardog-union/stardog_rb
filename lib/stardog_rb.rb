fdea700ef9cc3c6cce36eed5681905b6d23a5b5f
Cullan Springstead <cullan.springstead@stardog.com>
2019-01-07 12:49:01 -0600

    list databases and add a new database

--- lib/stardog_rb.rb
+++ lib/stardog_rb.rb
@@ -1,6 +1,7 @@
 require 'stardog_rb/version'
 require 'stardog_rb/connection'
 require 'stardog_rb/server'
+require 'stardog_rb/db/db'
 
 module StardogRb
   class Error < StandardError; end
---
+++ lib/stardog_rb/db/db.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+module StardogRb
+  module Db
+    # Control databases on the Stardog server
+    class Db
+      class << self
+        def list(conn)
+          request = conn.get_request('admin', 'databases')
+          conn.response(request, 'application/json')
+        end
+
+        def create(conn, database, options = {})
+          form_data = {
+            'root' => {
+              'dbname' => database,
+              'options' => options
+            }
+          }
+          request = conn.post_multipart_request(form_data, 'admin', 'databases')
+          conn.response(request, 'application/json')
+        end
+      end
+    end
+  end
+end
