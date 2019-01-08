d0981c4f59f5e7cc8b43f47aa91e65cbc848d433
Cullan Springstead <cullan.springstead@stardog.com>
2019-01-08 13:25:40 -0600

    test Db class

---
+++ test/stardog_rb/db/db_test.rb
@@ -0,0 +1,40 @@
+require 'json'
+require 'test_helper'
+require 'stardog_rb'
+
+class DbTest < Minitest::Test
+  def setup
+    @conn = StardogRb::Connection.new
+    VCR.insert_cassette name
+  end
+
+  def teardown
+    VCR.eject_cassette
+  end
+
+  def test_db_list
+    response = StardogRb::Db::Db.list(@conn)
+    response_json = JSON.parse(response.body)
+    assert response.code == '200'
+    assert response.content_type = 'application/json'
+    assert response_json.key?('databases')
+  end
+
+  def test_db_create
+    response = StardogRb::Db::Db.create(@conn, 'beatles')
+    response_json = JSON.parse(response.body)
+    message = response_json['message']
+    assert response.code == '201'
+    assert response.content_type = 'application/json'
+    assert message.start_with?('Successfully created database')
+  end
+
+  def test_db_drop
+    response = StardogRb::Db::Db.drop(@conn, 'beatles')
+    response_json = JSON.parse(response.body)
+    message = response_json['message']
+    assert response.code == '200'
+    assert response.content_type = 'application/json'
+    assert message.end_with?('was successfully dropped.')
+  end
+end
